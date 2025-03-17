f67dd7e1c5182824be08652139f3e6a0cef0662d
niassagd <emailgchang@gmail.com>
2025-03-16 22:31:45 -0400

    Fix nested eventListeners to stop new Book objects from duplicating.

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const harryPotter1 = {
     title: "Harry Potter 1",
     author: "J. K. Rowling",
     pages: 250,
-    read: true,
+    read: "read",
     id: 54343,
 }
 
@@ -12,7 +12,7 @@ const catInTheHat = {
     title: "Cat In The Hat",
     author: "Suess",
     pages: 20,
-    read: false,
+    read: "not read",
     id:36363,
 }
 
@@ -41,17 +41,119 @@ function createBooks () {
     for (let i = 0; i < myLibrary.length; i++) {    
         const book = document.createElement("div");
         book.classList.add("book");
-        book.textContent = myLibrary[i].title;
+
+        const bookTitle = document.createElement("p");
+        bookTitle.textContent = myLibrary[i].title;
+        bookTitle.classList.add("bookTitle");
+        book.appendChild(bookTitle);
+
+        const bookAuthor = document.createElement("p");
+        bookAuthor.textContent = `by ${myLibrary[i].author}`;
+        bookAuthor.classList.add("bookAuthor");
+        book.appendChild(bookAuthor);
+
+        const bookPages = document.createElement("p");
+        bookPages.textContent = `${myLibrary[i].pages} pages`;
+        bookPages.classList.add("bookPages");
+        book.appendChild(bookPages);
+
+        //variables for read and unread
+
+        const bookRead = document.createElement("p");
+        bookRead.textContent = myLibrary[i].read;
+        bookRead.classList.add("bookRead");
+        book.appendChild(bookRead);
+        
+        bookRead.classList.add("bookRead");
+        book.appendChild(bookRead);
         
         library.appendChild(book);
     };    
 }
 
 
+
+
 //create form to add new book
 
 const form = document.querySelector("form");
 const submitMessage = document.createElement("p");
+
+//new book title
+
+const newTitle = document.createElement("input");
+newTitle.id = "title";
+newTitle.name = "title";
+newTitle.type = "text";
+newTitle.required = true;
+
+const titleLabel = document.createElement("label");
+titleLabel.textContent = "Title: ";
+titleLabel.for = "title";
+
+//new book author
+const newAuthor = document.createElement("input");
+newAuthor.id = "author";
+newAuthor.name = "author";
+newAuthor.type = "text";
+newAuthor.required = true;
+
+const authorLabel = document.createElement("label");
+authorLabel.for = "author";
+authorLabel.textContent = "Author: "
+
+//new book pages
+
+const newPages = document.createElement("input");
+        newPages.id = "pages";
+        newPages.name = "pages";
+        newPages.type = "number";
+        newPages.required = true;
+    
+        const pagesLabel = document.createElement("label");
+        pagesLabel.for = "pages";
+        pagesLabel.textContent = "Number of Pages: ";
+    
+//form inputs for read or not read status
+
+const newRead = document.createElement("fieldset");
+
+
+const readStatus = document.createElement("legend");
+readStatus.textContent = "Have you already read this book?";
+
+
+//radio button and label for "read"
+const hasRead = document.createElement("input");
+hasRead.type = "radio";
+hasRead.name = "bookStatus";
+hasRead.id = "hasRead";
+hasRead.value = "read";
+hasRead.required = true;
+
+const readLabel = document.createElement("label");
+readLabel.for = "bookStatus";
+readLabel.textContent = "yes";
+
+//radio button and label for "not read"
+const hasNotRead = document.createElement("input")
+hasNotRead.type = "radio";
+hasNotRead.name = "bookStatus";
+hasNotRead.id = "hasNotRead";
+hasNotRead.value = "not read";
+
+const notReadLabel = document.createElement("label");
+notReadLabel.for = "bookStatus";
+notReadLabel.textContent = "no";
+
+//create submit button to add book
+    
+const addBook = document.createElement("input");
+addBook.type = "submit";
+addBook.value = "Add Book";
+addBook.classList.add(".addBook")
+
+// button to open form for new book 
 
 const newBookButton = document.querySelector('.newBookBtn');
 newBookButton.addEventListener("click", () => {
@@ -61,125 +163,77 @@ newBookButton.addEventListener("click", () => {
         }
         form.id = "myForm";
 
-        const newTitle = document.createElement("input");
-        newTitle.id = "title";
-        newTitle.name = "title";
-        newTitle.type = "text";
-        newTitle.required = true;
         
-        const titleLabel = document.createElement("label");
-        titleLabel.textContent = "Title: ";
-        titleLabel.for = "title";
     
         form.appendChild(titleLabel);
         titleLabel.appendChild(newTitle);
         
-        const newAuthor = document.createElement("input");
-        newAuthor.id = "author";
-        newAuthor.name = "author";
-        newAuthor.type = "text";
-        newAuthor.required = true;
-    
-        const authorLabel = document.createElement("label");
-        authorLabel.for = "author";
-        authorLabel.textContent = "Author: "
     
         form.appendChild(authorLabel);
         authorLabel.appendChild(newAuthor);
-    
-        const newPages = document.createElement("input");
-        newPages.id = "pages";
-        newPages.name = "pages";
-        newPages.type = "number";
-        newPages.required = true;
-    
-        const pagesLabel = document.createElement("label");
-        pagesLabel.for = "pages";
-        pagesLabel.textContent = "Number of Pages: ";
         
         form.appendChild(pagesLabel);
         pagesLabel.appendChild(newPages);
 
-        //form inputs for read or not read status
-
-        const newRead = document.createElement("fieldset");
         form.appendChild(newRead);
-        
-        const readStatus = document.createElement("legend");
-        readStatus.textContent = "Have you already read this book?";
+
         newRead.appendChild(readStatus);
-
-        //radio button and label for "read"
-        const hasRead = document.createElement("input");
-        hasRead.type = "radio";
-        hasRead.name = "readStatus";
-        hasRead.id = "hasRead";
-        hasRead.value = "read";
-        hasRead.required = true;
-
-        const readLabel = document.createElement("label");
-        readLabel.for = "read-status";
-        readLabel.textContent = "yes";
 
         newRead.appendChild(readLabel);
         readLabel.appendChild(hasRead);
 
-        //radio button and label for "not read"
-        const hasNotRead = document.createElement("input")
-        hasNotRead.type = "radio";
-        hasNotRead.name = "readStatus";
-        hasNotRead.id = "hasNotRead";
-        hasNotRead.value = "not read";
-
-        const notReadLabel = document.createElement("label");
-        notReadLabel.for = "read-status";
-        notReadLabel.textContent = "no";
+        
 
         newRead.appendChild(notReadLabel);
         notReadLabel.appendChild(hasNotRead);
 
-        //submit button ro add book
-    
-        const addBook = document.createElement("input");
-        addBook.type = "submit";
-        addBook.value = "Add Book";
-        addBook.classList.add(".addBook")
+        
         form.appendChild(addBook);
 
-        //Click Add book button to create new Book object and Add to library array
-        form.addEventListener("submit", function(event) {
-            event.preventDefault();
-
-            const title = document.getElementById("title").value;
-            console.log(title);
-
-            const author = document.getElementById("author").value;
-            console.log(author);
-
-            const pages = document.getElementById("pages").value;
-            console.log(pages);
-            
-            myLibrary.push(new Book(title, author, pages, true));
-            createBooks();
-
-            //remove inputs
-
-            newTitle.remove();
-            titleLabel.remove();
-            newAuthor.remove();
-            authorLabel.remove();
-            newPages.remove();
-            pagesLabel.remove();
-            addBook.remove();
-            newRead.remove();
-
-            const myMessage = "Your book has been added to the library!";
-            submitMessage.innerHTML = myMessage;
-            form.appendChild(submitMessage);
-            form.id = "message";
-        });
+        
     }
 })
 
 
 
+//Click Add book button to create new Book object and Add to library array
+form.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    const title = document.getElementById("title").value;
+    console.log(title);
+
+    const author = document.getElementById("author").value;
+    console.log(author);
+
+    const pages = document.getElementById("pages").value;
+    console.log(pages);
+
+    const bookStatus = document.getElementsByName("bookStatus").value;
+    console.log(bookStatus);
+    
+    myLibrary.push(new Book(title, author, pages, bookStatus));
+    createBooks();
+
+    //reset form
+
+    form.reset();
+
+    //remove inputs
+
+    newTitle.remove();
+    titleLabel.remove();
+    newAuthor.remove();
+    authorLabel.remove();
+    newPages.remove();
+    pagesLabel.remove();
+    addBook.remove();
+    newRead.remove();
+
+    
+
+    const myMessage = "Your book has been added to the library!";
+    submitMessage.innerHTML = myMessage;
+    form.appendChild(submitMessage);
+    form.id = "message";
+});
